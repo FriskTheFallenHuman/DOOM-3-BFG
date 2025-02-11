972ddcfb2a55932ca2d8e41d0863c37f58c1e8a1
Krispy <nikodash2020@hotmail.com>
2025-02-11 11:28:08 -0500

    Replace MSVC projects with premake5

---
+++ neo/.premake/modules.lua
@@ -0,0 +1,77 @@
+--[[						MIT License
+
+						Copyright (c) 2025 Krispy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. ]]
+
+--[[
+	Adress Stack Size
+	@purpose: Sets the size of  both stack and commit stack (MSVC)
+	@inputs: int size, int commit
+]]
+function adressstacksize(size,commit)
+	-- Not valid for StaticLibs
+    if not (_OPTIONS['target']) or _OPTIONS['target'] == 'StaticLib' then
+        return
+    end
+
+    buildoptions({"/STACK:" .. size .. "," .. commit})
+end
+
+--[[
+	Extra optimizations
+	@purpose: Sets even more optimizations that are not covered by premake's optimize()
+	@inputs: none
+]]
+function extraoptimization()
+	-- Not valid for StaticLibs
+    if not (_OPTIONS['target']) or _OPTIONS['target'] == 'StaticLib' then
+        return
+    end
+
+	linkoptions({"/OPT:REF", "/OPT:ICF"})
+end
+
+--[[
+	Link Time Optimizations Extended
+	@purpose: Replacement for linktimeoptimization for windows mainly
+	@inputs: none
+]]
+function linktimeoptimizationex(value)
+	-- Not valid for StaticLibs
+	if not (_OPTIONS['target']) or _OPTIONS['target'] == 'StaticLib' then
+		return
+	end
+
+	linkoptions({"/LTCG:incremental"})
+end
+
+--[[
+	Link Time Optimizations Extended
+	@purpose: Replacement for linktimeoptimization for windows mainly
+	@inputs: none
+]]
+function linktimeoptimizationex(value)
+	-- Not valid for StaticLibs
+	if not (_OPTIONS['target']) or _OPTIONS['target'] == 'StaticLib' then
+		return
+	end
+
+	linkoptions({"/LTCG:incremental"})
+end
---
+++ neo/premake5.lua
@@ -0,0 +1,134 @@
+--[[						MIT License
+
+						Copyright (c) 2025 Krispy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. ]]
+
+include(".premake/modules.lua")
+
+newoption {
+   trigger     = "dedicated",
+   description = "Build the dedicated server"
+}
+
+newoption {
+   trigger     = "dll",
+   description = "override the built-in game code with any custom gamex86.dll"
+}
+
+workspace("Doom3")
+	configurations({"Debug", "Release", "Retail"})
+	platforms("Win32")
+	location("../build")
+	preferredtoolarchitecture("x86")
+	--warnings( "Extra" )
+	--floatingpoint( "Fast" )
+	characterset( "ASCII" )
+
+	includedirs({".", "external", "idlib"})
+	defines({"__DOOM__", "ID_PC"})
+	if _OPTIONS["dll"] then
+		defines({"__DOOM_DLL__"})
+	end
+
+	filter("platforms:Win32")
+		system("Windows")
+		architecture("x86")
+		objdir("../build/out")
+	filter({})
+
+	-- _Common.props
+	filter("system:Windows")
+		defines({"_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_DEPRECATE", "_USE_32BIT_TIME_T"})
+		warnings("Extra")
+		flags({"MultiProcessorCompile"})
+
+		-- _Debug.props
+		filter("configurations:Debug")
+			optimize("Off")
+			defines("_DEBUG")
+			buildoptions({"/RTC1"})
+			flags({"Maps"})
+			staticruntime("On")
+			runtime("Debug")
+			rtti("On")
+			editandcontinue("On")
+		filter({}) -- Debug Scope
+
+		-- _Release.props
+		filter("configurations:Release")
+			optimize("Speed")
+			defines("NDEBUG")
+			stringpooling("On")
+			buildoptions({"/Ob2", "/Gy"})
+			linktimeoptimizationex("On")
+			intrinsics("On")
+			staticruntime("On")
+			runtime("Release")
+			flags({"NoBufferSecurityCheck"})
+			rtti("On")
+			editandcontinue("On")
+			extraoptimization()
+		filter({}) -- Release Scope
+
+		-- _DoomEXE.props
+		adressstacksize(16777216,16777216)
+		filter("kind:not StaticLib")
+			largeaddressaware("On")
+		filter({})
+
+		-- _PCLibs.props
+		filter("kind:not StaticLib")
+			links({"dbghelp", "glu32", "iphlpapi", "odbc32", "odbccp32", "opengl32", "winmm", "wsock32"}) -- Windows Libs
+			links({"xinput", "dinput8", "dsound", "dxguid"}) -- DirectX Libs
+		filter({})
+	filter({}) -- Windows Scope
+
+	include("amplitude/amplitude.lua")
+	include("external/external.lua")
+	include("idlib/idlib.lua")
+	if _OPTIONS["dll"] then
+		include("d3xp/d3xp.lua")
+	end
+
+	-- DoomEXE
+	group("exes")
+
+	project("DoomEXE")
+		targetname("Doom3BFG")
+		kind("WindowedApp")
+		language("C++")
+		links({"external", "idLib"})
+
+		pchsource("framework/precompiled.cpp")
+		pchheader("")
+
+		if _OPTIONS["dll"] then
+			files({"premake5.lua", "aas/**", "cm/**", "framework/**", "renderer/**", "sound/**", "swf/**", "sys/**", "ui/**" } )
+			removefiles({"framework/Session.cpp", "ui/GameWindow.cpp", "sys/win32/win_stats.*", "sys/win32/win_snd.cpp", "sys/win32/win_gamma.cpp", "renderer/BoundsTrack.cpp"})
+		else
+			files({"premake5.lua", "aas/**", "cm/**", "d3xp/**", "framework/**", "renderer/**", "sound/**", "swf/**", "sys/**", "ui/**" } )
+			removefiles({"d3xp/precompiled.cpp", "d3xp/EndLevel.*", "d3xp/gamesys/Callbacks.cpp", "d3xp/menus/MenuWidget_DevList.cpp", "framework/Session.cpp", "ui/GameWindow.cpp", "sys/win32/win_stats.*", "sys/win32/win_snd.cpp", "sys/win32/win_gamma.cpp", "renderer/BoundsTrack.cpp"})
+		end
+
+		filter("files:renderer/jobs/**.cpp")
+			flags({"NoPCH"})
+		filter({})
+
+	group("")
