7f292e3a90d991409128452f377d80e4fa1b35eb
Krispy <nikodash2020@hotmail.com>
2025-02-26 20:21:57 -0500

    Fixed the autosave dialog being stuck on the screen when starting a map (map or devmap) via the command line, or before selecting Doom 3.

--- neo/framework/Common_load.cpp
+++ neo/framework/Common_load.cpp
@@ -906,11 +906,11 @@ HandleCommonErrors
 ========================
 */
 bool HandleCommonErrors( const idSaveLoadParms & parms ) {
+	common->Dialog().ShowSaveIndicator( false );
+
 	if ( parms.GetError() == SAVEGAME_E_NONE ) {
 		return true;
 	}
-
-	common->Dialog().ShowSaveIndicator( false );
 
 	if ( parms.GetError() & SAVEGAME_E_CORRUPTED ) {
 		// This one might need to be handled by the game
