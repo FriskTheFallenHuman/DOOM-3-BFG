8f6de33baaafcbb4a871314bdc4502cd88b4d4a5
Krispy <nikodash2020@hotmail.com>
2026-04-05 15:24:38 -0500

    The engine now compiles under C++17

--- neo/sys/win32/win_localuser.h
+++ neo/sys/win32/win_localuser.h
@@ -45,6 +45,12 @@ public:
 
 	idLocalUserWin() : inputDevice( 0 ) {}
 
+	idLocalUserWin& operator=( idLocalUserWin &&other )	{
+		gamertag = std::move( other.gamertag );
+		inputDevice = other.inputDevice;
+		return *this;
+	}
+
 	//==========================================================================================
 	// idLocalUser interface
 	//==========================================================================================
